5b21b75cd10d3c3ebe35c6f3adf6b8b53613a2bd
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-04 10:13:15 -0400

    instantiates variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 const URL = "https://akabab.github.io/starwars-api/api/id/";
+
+const $input = $(`input[type="text"]`);
+const $form = $("form");
+const $charName = $("#charName");
+const $specName = $("#specName");
+const $height = $("#height");
+const $mass = $("#mass");
+const $birthYear = $("#birthYear");
+const $homeWorld = $("#homeWorld");
+
