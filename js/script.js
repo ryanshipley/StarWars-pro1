da976412695042fb5940e5d00430eea360fc65de
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-04 11:04:13 -0400

    adds pseudocode

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
 const allURL = "https://akabab.github.io/starwars-api/api/all.json"; //Instatiates the URL variable.
 
-const $input = $(`input[type="text"]`);
-const $doIt = $("#do-it");
-const $charName = $("#charName");
-const $specName = $("#specName");
-const $height = $("#height");
-const $mass = $("#mass");
-const $birthYear = $("#birthYear");
-const $homeWorld = $("#homeWorld");
-const $eyeColor = $("#eyeColor");
-const $affiliations = $("#affiliations");
+/*
+Instantiate all variables used
+*/
+const $input = $(`input[type="text"]`); //Sets the UI.
+const $doIt = $("#do-it"); //Creates a form with an image as the button.
+const $charName = $("#charName"); //Sets the character name.
+const $specName = $("#specName"); //sets the character species.
+const $height = $("#height"); //Sets the character height.
+const $mass = $("#mass"); //Sets the character mass.
+const $birthYear = $("#birthYear"); //Sets the character birth year.
+const $homeWorld = $("#homeWorld"); //Sets the character home world.
+const $eyeColor = $("#eyeColor"); //Sets the character eye color.
+const $affiliations = $("#affiliations"); //Sets the characters affiliations.
 
-$doIt.on("click", handleGetData);
-let characterData;
+$doIt.on("click", handleGetData);//Creates onclick function.
+let characterData; //Instantiates the character information.
 
+/*handleGetData function
+Retrieves the API data, creates a random varable, and finds the index of the data array 
+that matches that number.
+@param event the parameter for the functon
+@return call the AJAX method.
+*/
 function handleGetData(event){
     event.preventDefault();
 
+/*AJAX method
+@param data the data from the API
+@randomChar the random number created by the method.
+@log console the randomChar.
+@characterData The data for the randm matching character.
+@find find the element in the object whose id matches that random number.
+@log console the characterData for the matching character.
+@render calls the render function.
+@changeImage calls the changeImage function.
+@textCrawl calls the textCrawl function.
+@error runs an error if there is an error.
+*/
 $.ajax(allURL).then((data) =>{
     allData = data;
     let randomChar = Math.floor(Math.random() * allData.length);
@@ -30,6 +51,9 @@ $.ajax(allURL).then((data) =>{
     });
 };
 
+/*Render function
+When called, it sets the text for the HTML elements to be the data extracted in the data.
+*/
 function render(){
     $charName.text(characterData.name);
     $specName.text(characterData.species);
@@ -41,11 +65,17 @@ function render(){
     $affiliations.text(characterData.affiliations);
 };
 
+/*changeImage function
+Changes the image of the character based on the random search
+*/
 function changeImage(){
     document.getElementById("photo").src = characterData.image;
     document.getElementById("photo").alt = characterData.name;
  };
 
+/*textCrawl function
+Changes the styling of the character card and makes it crawl.
+*/
  function textCrawl(){
     $("#card").css({
         "height": "100%",
