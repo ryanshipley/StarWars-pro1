169cd02222f17aae7165129fa6bd93e8e0ef4b0c
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-04 10:33:40 -0400

    fixes character card

--- js/script.js
+++ js/script.js
@@ -9,7 +9,20 @@ const $mass = $("#mass");
 const $birthYear = $("#birthYear");
 const $homeWorld = $("#homeWorld");
 
+$form.on("submit", handleGetData);
+let characterData;
+
+function handleGetData(event){
+    event.preventDefault();
+    userInput = $input.val();
+    if(userInput === "") return;
 $.ajax(URL + 1 + ".json").then((data) =>{
-    characterData = data;
-    console.log(characterData);
-});
+        characterData = data;
+        render();
+        $("body").append(`<img id="photo" src="${characterData.image}"`);
+        $input.val("");
+    }, (error) =>{
+        console.log("ERROR");
+        console.log(error);
+    });
+};
